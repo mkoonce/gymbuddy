85ed432b0a123d352644a4e12fbb47a419f6f48c
Sara Millar <snmillar@ucsd.edu>
2014-02-20 19:59:34 -0800

    changed name in breadcrumbs using url get

--- routes/entry.js
+++ routes/entry.js
@@ -1,3 +1,20 @@
+var workouts = require('../workouts.json');
+
 exports.view = function(req, res){
-	res.render('entry');
+	var url = require('url');
+	var url_parts = url.parse(req.url, true);
+	var workoutPath = url_parts.query['lastPage'];
+
+	var data = ({});
+
+	var i;
+	for(i=0; i<workouts.length; i++){
+		if(workouts[i].path == workoutPath){
+			data = workouts[i];
+		}
+	}
+
+	//console.log(data);
+
+	res.render('entry', data);
 };
