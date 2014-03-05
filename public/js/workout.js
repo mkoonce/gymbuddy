6b790146c7231ebd7f89243575729835150fe8ae
Sara Millar <snmillar@ucsd.edu>
2014-03-04 18:31:36 -0800

    google analytics experiment

---
+++ public/js/workout.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    initializePage();
+});
+
+function initializePage() {
+	$(".safety").click(sendSafety);
+	$(".howto").click(sendHowto);
+	$(".beginner").click(sendBeginner);
+	$(".intermediate").click(sendIntermediate);
+	$(".advanced").click(sendAdvanced);
+	$(".entry").click(sendEntry);
+}
+
+function sendSafety(e){
+	e.preventDefault();
+	ga('send', 'event', 'workoutsafety', 'click');
+}
+
+function sendHowto(e){
+	e.preventDefault();
+	ga('send', 'event', 'workouthowto', 'click');
+}
+
+function sendBeginner(e){
+	e.preventDefault();
+	ga('send', 'event', 'workoutbeginner', 'click');
+}
+
+function sendIntermediate(e){
+	e.preventDefault();
+	ga('send', 'event', 'workoutintermediate', 'click');
+}
+
+function sendAdvanced(e){
+	e.preventDefault();
+	ga('send', 'event', 'workoutadvanced', 'click');
+}
+
+function sendEntry(e){
+	e.preventDefault();
+	ga('send', 'event', 'workoutentry', 'click');
+}
